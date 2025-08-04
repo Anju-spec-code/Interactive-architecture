a9568b3f3a2dd85201875807c3d2df9379d0c394
Anju-spec-code <chaudhary.anju15@gmail.com>
2025-08-04 11:47:55 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,1 @@
-const diagram = {
-  title: "Test",
-  subtitle: "",
-  footer: "",
-  showGrid: false,
-  groups: [],
-  nodes: [{ id:"n1", label:"Hello", x:100, y:100, w:200, h:80 }],
-  edges: []
-};
+const diagram={title:"Test",subtitle:"",footer:"",showGrid:false,groups:[],nodes:[{id:"n1",label:"Hello",x:100,y:100,w:200,h:80}],edges:[]};
